6f2f79a93931f2001ac6404d800df570abf552fc
SergioVidiellaPinto <sergiovp23@gmail.com>
2018-06-05 22:03:03 +0200

    Implement POST methods

--- app/main.py
+++ app/main.py
@@ -21,15 +21,57 @@ def update_submissions():
 
 @app.route('/api/v1.0/classify', methods=['POST'])
 def classify_input():
-    print(request.json)
+    request.get_json(force=True)
     if not request.json or not 'inputs' in request.json:
             abort(400)
+    
+    input_data = np.array(request.json['inputs'])
+    input_attributes = input_data[:, 1:10]
+    predictions = classifier.make_prediction(input_attributes)
+    res = []
+    
+    for idx, pred in zip(input_data[:, 0], predictions):
+        res.append({"id": idx, "class": pred})
         
-    pred = classifier.make_prediction(np.array(request.json['inputs']))
-    prediction = {
-        'result': pred
+    result = {
+        'result': res
     }
-    return jsonify({'task': prediction})
+    
+    return jsonify(result)
+
+
+@app.route('/api/v1.0/prediction', methods=['POST'])
+def classify_input():
+    request.get_json(force=True)
+    if not request.json or not 'id' in request.json \
+    or not 'clump_thickness' in request.json \
+    or not 'uniformity_cell_size' in request.json \
+    or not 'uniformity_cell_shape' in request.json \
+    or not 'marginal_adhesion' in request.json \
+    or not 'single_ephithetial_cell_size' in request.json\
+    or not 'bare_nuclei' in request.json \
+    or not 'bland_chromatin' in request.json \
+    or not 'normal_nucleoli' in request.json or not 'mitoses' in request.json:
+        abort(400)
+    
+    input_array = np.array([[request.json['clump_thickness'], 
+                             request.json['uniformity_cell_size'], 
+                             request.json['uniformity_cell_shape'],
+                             request.json['marginal_adhesion'], 
+                             request.json['single_ephithetial_cell_size'], 
+                             request.json['bare_nuclei'],
+                             request.json['bland_chromatin'], 
+                             request.json['normal_nucleoli'], 
+                             request.json['mitoses']]])
+    
+    prediction = classifier.make_prediction(input_array)
+        
+    result = {
+        'id': request.json['id'],
+        'class': prediction[0]
+    }
+    
+    return jsonify(result)
 
 
 if __name__ == '__main__':
